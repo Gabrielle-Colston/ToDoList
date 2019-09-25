07697d7b0925f0d266608b7a2fbea2604ef9e43b
Gabrielle Lee <Gabrielle.Lee@openlink.com>
2019-09-25 09:56:47 -0500

    Code through video 223

--- js/ToDoList.js
+++ js/ToDoList.js
@@ -1,1 +1,12 @@
+$("li").on("click", function() {
+    $(this).toggleClass("completed");
+})
 
+$("span").on("click", function(e) {
+    $(this).parent().fadeOut(500,
+        function() {
+            $(this).remove();
+        });
+
+    e.stopPropagation();
+})
